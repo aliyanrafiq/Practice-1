daf191952f4198da7439790712b3b2f47387cc69
Muhammad Aliyan <120269166+aliyanrafiq@users.noreply.github.com>
2024-12-04 18:25:51 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+// ===== Chapter # 01 Starts Here =====
+
+// // Question # 01
+// alert("Welcome Aliyan")
+
+// // Question # 02
+// alert("Please Enter a valid password")
+
+// // Question # 03
+// alert("Welcome to JS land... \n Happy Coding!")
+
+// // Question # 04
+// alert("Welcome to JS land...");
+// alert("Happy Coding!");
+
+// // Question # 05
+// console.log("Hello... I can run my JS through my web browser's console")
+
+// ===== Chapter # 01 Ends Here =====
+
+
+// ===== Chapter # 02 Starts Here =====
+
+// // Question # 01
+// var username;
+
+// // Question # 02
+// var myName = "Muhammad Aliyan";
+
+// // Question # 03
+// var message = "Hello World";
+// alert(message);
+
+// // Question # 04
+// var name = "Muhammad Aliyan";
+// var age = "21 years old";
+// var certification = "Certified Web Development";
+// alert(name);
+// alert(age);
+// alert(certification);
+
+// // Question # 05
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(pizza);
+
+// // // Question # 06
+// var email = "aliyanattariqadri@gmail.com";
+// alert("My Email address is " + email);
+
+// // Question # 07
+// var book = "A Smarter Way To Learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// // Question # 08
+// document.write("Yay I can write HTML content through JavaScript");
+
+// // Question # 08
+// var pic = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(pic);
+
+// ===== Chapter # 02 Ends Here =====
+
+
+// ===== Chapter # 03 Starts Here =====
+
+// // Question # 01
+// var age = 21;
+// alert("I am " + age + " years old");
+
+// // Question # 02
+// var visited = 14;
+// alert("You have visited this site " + visited + " times");
+
+// // Question # 03
+// var birthYear = 2003;
+// document.write("My Birth year is " + birthYear + "<br /> Data type of my declared variable is number")
+
+// // Question # 04
+// var visitorName = "Muhammad Aliyan";
+// var productTitle = "T-Shirt";
+// var quantity = 5;
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing Store");
+
+// ===== Chapter # 03 Ends Here =====
